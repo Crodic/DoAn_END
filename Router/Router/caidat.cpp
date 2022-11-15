21ad16ce60a4e57bfd9603164e67e1669e00fde5
Crodic <113219638+Crodic@users.noreply.github.com>
2022-11-15 15:50:14 +0700

    Add files via upload

---
+++ Router/Router/caidat.cpp
@@ -0,0 +1,79 @@
+﻿#include"Header.h"
+int trongso[MAX][MAX]; //trọng số từ x đến y trong khoảng MAX
+int soRouter; //Số router của mạng
+#pragma region Đọc Ma Trận Mạng Router
+void docmatran()
+{
+	FILE* f;
+	fopen_s(&f, "router.txt", "rt"); //Mở file router.txt chế độ đọc văn bản.
+	if (f == NULL)
+	{
+		return;
+	}
+	fscanf_s(f, "%d", &sonut); //Đọc từng dòng file
+	for (int i = 0; i < sonut; i++)
+	{
+		for (int j = 0; j < sonut; j++)
+			fscanf_s(f, "%d", &trongso[i][j]);
+	}
+	fclose(f);
+}
+#pragma endregion
+#pragma region Xem Mạng Máy Tính
+void xemmatran()
+{
+	cout << "Ma Tran Mang May Tinh La: \n";
+	for (int i = 0; i < sonut; i++)
+	{
+		for (int j = 0; j < sonut; j++)
+			cout << trongso[i][j] << "\t";
+		cout << endl;
+	}
+	cout << "____________________________________________ \n";
+}
+#pragma endregion
+#pragma region Tìm Đường Đi Ngắn Nhất
+void router(int routerNguon, int routerDich, int* ngan, int duongdi[]) {
+	int capnut[MAX]; //Đánh dấu đường đi đã được xét hay chưa
+	int kcbandau[MAX]; //Đánh dấu khoảng cách giữa 2 router
+	int routerTram; //Router hiện tại đang xét
+	int min; //Giá trị nhỏ nhất tạm thời
+
+	//Khởi tạo các giá trị tạm thời cho các đỉnh.
+	for (int i = 0; i < soRouter; i++) {
+		capnut[i] = FALSE;
+		kcbandau[i] = vocung;
+		duongdi[i] = routerNguon;
+	}
+	//Cho khoảng cách từ router nguồn đến chính nó bằng 0 và đánh dấu router đó
+	capnut[routerNguon] = TRUE;
+	kcbandau[routerNguon] = 0;
+	routerTram = routerNguon;
+	while (routerTram != routerDich) 
+	{
+		min = vocung;
+		//Duyệt các đỉnh cần xét trong đồ thị
+		for (int j = 0; j < soRouter; j++) 
+		{
+			//Kiểm tra các điều kiện về đường đi, khoảng cách, và đỉnh đó đã được xét hay chưa
+			if (trongso[routerTram][j] > 0 && kcbandau[j] > trongso[routerTram][j] + kcbandau[routerTram] && capnut[j] == FALSE) 
+			{
+				//Cập nhật lại khoảng cách mới nếu khoảng cách mới bé hơn khoảng cách ban đầu và lưu lộ trình của đỉnh j.
+				kcbandau[j] = trongso[routerTram][j] + kcbandau[routerTram];
+				duongdi[j] = routerTram;
+			}
+			//Duyệt lại các đỉnh để trả về giá trị sau khi cập nhật và đánh dấu đỉnh đó đã được xét
+			for (int j = 0; j < soRouter; j++) 
+			{
+				if (min > kcbandau[j] && capnut[j] == FALSE)
+				{
+					min = kcbandau[j];
+					routerTram = j;
+				}
+				capnut[routerTram] = TRUE;
+			}
+		}
+	}
+	*ngan = kcbandau[routerDich]; //Lưu giá trị khoảng cách giữa 2 router vào biến *ngan
+}
+#pragma endregion
---
+++ Router/Router/chuongtrinh.cpp
@@ -0,0 +1,44 @@
+#include"Header.h"
+int main()
+{
+	int chon;
+	int routerNguon, routerDich;
+	int ngan;
+	int duongdi[MAX];
+	int i;
+	do
+	{
+		cout << "\n1. Xem Ma Tran Mang";
+		cout << "\n2. Kiem Tra Duong Truyen Ngan Nhat Giua 2 Router";
+		cout << "\n0. Thoat";
+		cout << "\n Chon chuc nang ban can: ";
+		cin >> chon;
+		switch (chon)
+		{
+			case 1:
+			{
+				docmatran();
+				xemmatran();
+				break;
+			}
+			case 2: 
+			{
+				docmatran();
+				cout << "\nNhap vao 1 Router bat ky: "; cin >> routerNguon;
+				cout << "\nNhap vao Router can di den: "; cin >> routerDich;
+				router(routerNguon, routerDich, &ngan, duongdi);
+				cout << "\nDuong truyen ngan nhat giua Router (" << routerNguon << ") den Router (" << routerDich << ") la: " << ngan;
+				cout << "\nLo trinh duy chuyen giua Router (" << routerNguon << ") den Router (" << routerDich << ") la: ";
+				i = routerDich;
+				while (i != routerNguon) 
+				{
+					cout << i << " -> ";
+					i = duongdi[i];
+				}
+				cout << routerNguon;
+				cout << "\n____________________________________________________________________________";
+				break;
+			}
+		}
+	} while (chon != 0);
+}
