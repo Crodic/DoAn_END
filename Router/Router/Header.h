870959f89915b4059c0421d79b01a8bbc03d05c4
Crodic <113219638+Crodic@users.noreply.github.com>
2022-11-22 14:51:11 +0700

    Delete Router directory

--- Router/Router/Header.h
+++
@@ -1,9 +0,0 @@
-#include"iostream"
-using namespace std;
-#define vocung 10000000
-#define MAX 50
-#define TRUE 1
-#define FALSE 0
-void docmatran();
-void xemmatran();
-void router(int routerNguon, int routerDich, int* ngan, int duongdi[]);
